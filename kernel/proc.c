d9a6072e8b4d0edc2fab6dace0a7fd252e0361fe
Hao Chen <501018786@qq.com>
2024-06-11 23:43:08 +0800

    traps: alarm

--- kernel/proc.c
+++ kernel/proc.c
@@ -141,6 +141,11 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  p->alarm_interval = 0;
+  p->ticks_passed = 0;
+  p->alarm_handler = 0;
+  p->handler_is_running = 0;
+
   return p;
 }
 
